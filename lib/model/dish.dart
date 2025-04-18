f7e8087c6f97b6292849b9c46ead8f1c2214f9b6
brianrecoba <brian12fernandes@gmail.com>
2025-04-17 22:56:23 -0300

    feat: create dish model

---
+++ lib/model/dish.dart
@@ -0,0 +1,15 @@
+class DishModel {
+  final String id;
+  final String name;
+  final String description;
+  final int price;
+  final String imagePath;
+
+  DishModel({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.price,
+    required this.imagePath,
+  });
+}
