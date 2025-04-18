95228cc15acc2af6c8fb3503d71e4df0b05c0665
brianrecoba <brian12fernandes@gmail.com>
2025-04-17 22:57:32 -0300

    feat: add the InkWell Widget

--- lib/home/widgets/restaurant_widget.dart
+++ lib/home/widgets/restaurant_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_mobile_techtaste/model/restaurant.dart';
+import 'package:mobile_mobile_techtaste/restaurant/restaurant_screen.dart';
 
 class RestaurantWidget extends StatelessWidget {
   final Restaurant restaurant;
@@ -7,32 +8,45 @@ class RestaurantWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      spacing: 12,
-      children: [
-        Image.asset(
-          'assets/${restaurant.imagePath}',
-          width: 72,
-        ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              restaurant.name,
-              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-            ),
-            Row(
-              children: List.generate(restaurant.stars.toInt(), (index) {
-                return Image.asset(
-                  'assets/others/star.png',
-                  width: 16,
-                );
-              }),
-            ),
-            Text('${restaurant.distance}km'),
-          ],
-        ),
-      ],
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return RestaurantScreen(restaurant: restaurant);
+            },
+          ),
+        );
+      },
+      child: Row(
+        spacing: 12,
+        children: [
+          Image.asset(
+            'assets/${restaurant.imagePath}',
+            width: 72,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                restaurant.name,
+                style:
+                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+              ),
+              Row(
+                children: List.generate(restaurant.stars.toInt(), (index) {
+                  return Image.asset(
+                    'assets/others/star.png',
+                    width: 16,
+                  );
+                }),
+              ),
+              Text('${restaurant.distance}km'),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
