184efc9ab18e1d52519c8d821e5b585b16d27519
brianrecoba <brian12fernandes@gmail.com>
2025-04-21 01:24:25 -0300

    feat: create toMap and fromMap in DishModel

--- lib/model/dish.dart
+++ lib/model/dish.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 class DishModel {
   final String id;
   final String name;
@@ -12,4 +14,29 @@ class DishModel {
     required this.price,
     required this.imagePath,
   });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'description': description,
+      'price': price,
+      'imagePath': imagePath
+    };
+  }
+
+  factory DishModel.fromMap(Map<String, dynamic> map) {
+    return DishModel(
+      id: map['id'],
+      name: map['name'],
+      description: map['description'],
+      price: map['price'],
+      imagePath: map['imagePath'],
+    );
+  }
+
+  @override
+  String toString() {
+    return 'Dish(id: $id, name: $name, description: $description, price: $price, imagePath: $imagePath)';
+  }
 }
