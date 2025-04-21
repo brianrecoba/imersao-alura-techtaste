3f6c583c2d9baa302ae7fe4a59a078f0cd59e48d
brianrecoba <brian12fernandes@gmail.com>
2025-04-21 01:25:01 -0300

    feat: add the list dishes in restaurant

--- lib/model/restaurant.dart
+++ lib/model/restaurant.dart
@@ -1,3 +1,5 @@
+import 'package:mobile_mobile_techtaste/model/dish.dart';
+
 class Restaurant {
   String id;
   String imagePath;
@@ -6,6 +8,7 @@ class Restaurant {
   double stars;
   int distance;
   List<String> categories;
+  List<DishModel> dishes;
 
   Restaurant(
       {required this.id,
@@ -14,7 +17,8 @@ class Restaurant {
       required this.description,
       required this.stars,
       required this.distance,
-      required this.categories});
+      required this.categories,
+      required this.dishes});
 
   Map<String, dynamic> toMap() {
     return {
@@ -24,7 +28,8 @@ class Restaurant {
       'description': description,
       'stars': stars,
       'distance': distance,
-      'categories': categories
+      'categories': categories,
+      'dishes': dishes.map((dishes) => dishes.toMap()).toList()
     };
   }
 
@@ -37,6 +42,9 @@ class Restaurant {
       stars: map['stars'],
       distance: map['distance'],
       categories: List<String>.from(map['categories']),
+      dishes: List<DishModel>.from(
+        map['dishes'].map((dishes) => DishModel.fromMap(dishes)),
+      ),
     );
   }
   @override
